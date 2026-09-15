4e6f82c8ace51238bd7beebbd6c5663a01834d10
Murilo Poltronieri <148149959+murilopbc@users.noreply.github.com>
2026-09-15 20:13:20 -0300

    Fix environment variable name for database URL

--- clinica_config/settings.py
+++ clinica_config/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-database_url = os.environ.get("DATABASES_URL")
+database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
 
 
